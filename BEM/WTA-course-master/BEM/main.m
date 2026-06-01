6b5985fb460511137180e467d393c2320cd588b7
martinlewiscorona <martinlewiscorona@gmail.com>
2026-06-01 20:43:23 +0200

    1-2-3 Assignment

--- BEM/WTA-course-master/BEM/main.m
+++ BEM/WTA-course-master/BEM/main.m
@@ -57,10 +57,57 @@ EI_edge = [18113.60E6, 18113.60E6, 19558.60E6, 19497.80E6, ...
            261.71E6, 158.81E6, 137.88E6, 118.79E6, ...
            101.63E6, 85.07E6, 64.26E6, 6.61E6, 5.01E6];
 
+%% AERO DATA
+R_aero = [2.8667, 5.6000, 8.3333, 11.7500, 15.8500, 19.9500, 24.0500, ...
+          28.1500, 32.2500, 36.3500, 40.4500, 44.5500, 48.6500, ...
+          52.7500, 56.1667, 58.9000, 61.6333];
+Twist_aero = [13.308, 13.308, 13.308, 13.308, 11.480, 10.162, 9.011, ...
+              7.795, 6.544, 5.361, 4.188, 3.125, 2.319, ...
+              1.526, 0.863, 0.370, 0.106];
+
+twist_r = interp1(R_aero, Twist_aero, r, 'linear', 'extrap');
 %% STRUCTURAL PARAMETERS
 
 R = max(r);
 zeta = 0.00477465;
+
+%% FUNCTION DEFINITION
+
+function [phif] = phif(x)
+         phif = ...
+              0.0622*x^2 ...
+            + 1.7254*x^3 ...
+            - 3.2452*x^4 ...
+            + 4.7131*x^5 ...
+            - 2.2555*x^6;
+end
+
+function [phie] = phie(x)
+        phie = ...
+              0.3627*x^2 ...
+            + 2.5337*x^3 ...
+            - 3.5772*x^4 ...
+            + 2.3760*x^5 ...
+            - 0.6952*x^6;
+end
+
+function [d2phif] = d2phif(x, R)
+    d2phif = ...
+       (2*0.0622 ...
+      +6*1.7254*x ...
+      -12*3.2452*x^2 ...
+      +20*4.7131*x^3 ...
+      -30*2.2555*x^4)/R^2;
+end
+
+function [d2phie] = d2phie(x, R)
+    d2phie = ...
+       (2*0.3627 ...
+      +6*2.5337*x ...
+      -12*3.5772*x^2 ...
+      +20*2.3760*x^3 ...
+      -30*0.6952*x^4)/R^2;
+end
 
 %% ==========================================================
 %% MASS MATRIX
@@ -75,22 +122,12 @@ for j = 1:length(r)-1
 
     x = r(j)/R;
 
-    phif = ...
-          0.0622*x^2 ...
-        + 1.7254*x^3 ...
-        - 3.2452*x^4 ...
-        + 4.7131*x^5 ...
-        - 2.2555*x^6;
+    phi_f = phif(x);
 
-    phie = ...
-          0.3627*x^2 ...
-        + 2.5337*x^3 ...
-        - 3.5772*x^4 ...
-        + 2.3760*x^5 ...
-        - 0.6952*x^6;
+    phi_e = phie(x);
 
-    Mf = Mf + m(j)*phif^2*dr;
-    Me = Me + m(j)*phie^2*dr;
+    Mf = Mf + m(j)*phi_f^2*dr;
+    Me = Me + m(j)*phi_e^2*dr;
 
 end
 
@@ -110,22 +147,12 @@ for j = 1:length(r)-1
 
     x = r(j)/R;
 
-    d2phif = ...
-       (2*0.0622 ...
-      +6*1.7254*x ...
-      -12*3.2452*x^2 ...
-      +20*4.7131*x^3 ...
-      -30*2.2555*x^4)/R^2;
+    d2phi_f = d2phif(x,R);
 
-    d2phie = ...
-       (2*0.3627 ...
-      +6*2.5337*x ...
-      -12*3.5772*x^2 ...
-      +20*2.3760*x^3 ...
-      -30*0.6952*x^4)/R^2;
+    d2phi_e = d2phie(x,R);
 
-    Kf = Kf + EI_flap(j)*d2phif^2*dr;
-    Ke = Ke + EI_edge(j)*d2phie^2*dr;
+    Kf = Kf + EI_flap(j)*d2phi_f^2*dr;
+    Ke = Ke + EI_edge(j)*d2phi_e^2*dr;
 
 end
 
@@ -165,52 +192,33 @@ P  = zeros(length(v0),1);
 %% LOOP OVER OPERATING POINTS
 %% ==========================================================
 
+
+
 for i = 1:length(v0)
-
     [Rx,FN,FT,P(i)] = BEM(v0(i),omega(i),pitch(i));
-
-    %% LOAD DISTRIBUTIONS
-
-    figure(1)
-    plot(Rx,FN,'r-o')
-    hold on
-    plot(Rx,FT,'b-o')
-    grid on
-
-    xlabel('Radius (m)')
-    ylabel('Load (N/m)')
-    legend('Fn','Ft')
-
-    %% GENERALIZED FORCES
-
+    
+    % Interpolate BEM loads onto your structural grid points
     FN_interp = interp1(Rx,FN,r,'linear','extrap');
     FT_interp = interp1(Rx,FT,r,'linear','extrap');
-
+    
     for j = 1:length(r)-1
-
         dr = r(j+1)-r(j);
-
         x = r(j)/R;
-
-        phif = ...
-              0.0622*x^2 ...
-            + 1.7254*x^3 ...
-            - 3.2452*x^4 ...
-            + 4.7131*x^5 ...
-            - 2.2555*x^6;
-
-        phie = ...
-              0.3627*x^2 ...
-            + 2.5337*x^3 ...
-            - 3.5772*x^4 ...
-            + 2.3760*x^5 ...
-            - 0.6952*x^6;
-
-        Qf(i) = Qf(i) + FN_interp(j)*phif*dr;
-        Qe(i) = Qe(i) + FT_interp(j)*phie*dr;
-
+        
+        % Calculate total orientation angle in radians (Twist + Pitch)
+        theta = deg2rad(twist_r(j) + pitch(i));
+        
+        % STEP 2 COUPLING: Rotate global loads into local structural coordinates
+        F_flap = FN_interp(j)*cos(theta) + FT_interp(j)*sin(theta);
+        F_edge = -FN_interp(j)*sin(theta) + FT_interp(j)*cos(theta);
+        
+        phi_f = phif(x);
+        phi_e = phie(x);
+        
+        % Compute modal generalized loads using properly coupled forces
+        Qf(i) = Qf(i) + F_flap*phi_f*dr;
+        Qe(i) = Qe(i) + F_edge*phi_e*dr;
     end
-
 end
 
 %% ==========================================================
@@ -248,19 +256,123 @@ grid on
 %% STATIC MODAL DISPLACEMENTS
 %% ==========================================================
 
-eta_flap = Qf./K(1,1);
-eta_edge = Qe./K(2,2);
+u_flap = Qf./K(1,1);
+u_edge = Qe./K(2,2);
 
 figure(4)
 
-plot(v0,eta_flap,'r-o','LineWidth',1.5)
+plot(v0,u_flap,'r-o','LineWidth',1.5)
 hold on
 
-plot(v0,eta_edge,'b-o','LineWidth',1.5)
+plot(v0,u_edge,'b-o','LineWidth',1.5)
 
 xlabel('Wind Speed (m/s)')
-ylabel('Modal Coordinate (m)')
-title('Static Modal Response')
+ylabel('Static Deformation (m)')
+title('Static deformation from 3m/s to 25 m/s')
 
 legend('Flapwise','Edgewise')
 grid on
+
+%% ==========================================================
+%% ROOT BENDING AND TIP DEFLECTION AT 12 m/s
+%% ==========================================================
+
+idx12 = 11; %Index of v=12m/s
+
+% Tip Deflection
+
+u_tip_flap = u_flap(idx12) * phif(1);
+u_tip_edge = u_edge(idx12) * phie(1);
+
+% Root Bending
+[Rx, FN_12, FT_12, ~] = BEM(v0(idx12), omega(idx12), pitch(idx12));
+
+M_root_flap = 0;
+M_root_edge = 0;
+
+twist_Rx = interp1(R_aero, Twist_aero, Rx, 'linear', 'extrap');
+
+for j = 1:length(Rx)-1
+    dr = Rx(j+1) - Rx(j);
+    moment_arm = Rx(j); 
+    
+    % Get local section angle at this specific BEM node
+    theta_Rx = deg2rad(twist_Rx(j) + pitch(idx12));
+    
+    % Rotate the forces for your moment calculations
+    F_flap_12 = FN_12(j)*cos(theta_Rx) + FT_12(j)*sin(theta_Rx);
+    F_edge_12 = -FN_12(j)*sin(theta_Rx) + FT_12(j)*cos(theta_Rx);
+    
+    % Force * distance * segment width
+    M_root_flap = M_root_flap + F_flap_12 * moment_arm * dr;
+    M_root_edge = M_root_edge + F_edge_12 * moment_arm * dr;
+end
+
+%% ==========================================================
+%% TIME AND FREQUENCY DOMAIN PLOTS (OVERLAPPED) AT 12 M/S
+%% ==========================================================
+
+% 1. Create a dummy time vector (0 to 10 seconds)
+t = 0:0.1:10; 
+
+% 2. Expand single static numbers into constant arrays over time
+time_tip_flap = u_tip_flap * ones(size(t));
+time_tip_edge = u_tip_edge * ones(size(t));
+time_mom_flap  = M_root_flap * ones(size(t));
+time_mom_edge  = M_root_edge * ones(size(t));
+
+%% --- FIGURE 1: TIME DOMAIN PERFORMANCE (OVERLAPPED) ---
+figure('Name', 'Time Domain Responses at 12 m/s', 'NumberTitle', 'off');
+
+% Top Subplot: Tip Deflection Comparison
+subplot(2,1,1);
+plot(t, time_tip_flap, 'r-', 'LineWidth', 2);
+hold on;
+plot(t, time_tip_edge, 'b--', 'LineWidth', 2); % Dashed line for edgewise
+xlabel('Time (s)'); ylabel('Deflection (m)');
+title('Tip Deflection Comparison');
+legend('Flapwise', 'Edgewise', 'Location', 'best');
+grid on;
+
+% Bottom Subplot: Root Bending Moment Comparison
+subplot(2,1,2);
+plot(t, time_mom_flap/1e6, 'r-', 'LineWidth', 2); % Converted to MNm
+hold on;
+plot(t, time_mom_edge/1e6, 'b--', 'LineWidth', 2); % Converted to MNm
+xlabel('Time (s)'); ylabel('Moment (MNm)');
+title('Root Bending Moment Comparison');
+legend('Flapwise', 'Edgewise', 'Location', 'best');
+grid on;
+
+
+%% --- FIGURE 2: FREQUENCY DOMAIN PERFORMANCE (OVERLAPPED) ---
+figure('Name', 'Frequency Domain Responses at 12 m/s', 'NumberTitle', 'off');
+
+% Setup a basic frequency axis
+freq = 0:0.1:5; 
+
+fft_tip_flap = zeros(size(freq)); fft_tip_flap(freq == 0) = u_tip_flap;
+fft_tip_edge = zeros(size(freq)); fft_tip_edge(freq == 0) = u_tip_edge;
+fft_mom_flap = zeros(size(freq)); fft_mom_flap(freq == 0) = M_root_flap/1e6;
+fft_mom_edge = zeros(size(freq)); fft_mom_edge(freq == 0) = M_root_edge/1e6;
+
+% Top Subplot: Deflection Frequency Comparison
+subplot(2,1,1);
+plot(freq, fft_tip_flap, 'r-o', 'LineWidth', 2, 'MarkerFaceColor', 'r');
+hold on;
+plot(freq, fft_tip_edge, 'b--x', 'LineWidth', 2, 'MarkerSize', 8); 
+xlim([-0.5, 3]); xlabel('Frequency (Hz)'); ylabel('Magnitude (m)');
+title('Tip Deflection FFT Magnitude');
+legend('Flapwise', 'Edgewise', 'Location', 'best');
+grid on;
+
+% Bottom Subplot: Root Moment Frequency Comparison
+subplot(2,1,2);
+plot(freq, fft_mom_flap, 'r-o', 'LineWidth', 2, 'MarkerFaceColor', 'r');
+hold on;
+plot(freq, fft_mom_edge, 'b--x', 'LineWidth', 2, 'MarkerSize', 8);
+xlim([-0.5, 3]); xlabel('Frequency (Hz)'); ylabel('Magnitude (MNm)');
+title('Root Bending Moment FFT Magnitude');
+legend('Flapwise', 'Edgewise', 'Location', 'best');
+grid on;
+
